ba99c671487b36bef499bfd24db52b20cf910949
Luke Whyte <lukeawhyte@gmail.com>
2015-01-26 23:48:27 -0800

    Adding UI

--- deal.js
+++ deal.js
@@ -3,21 +3,30 @@ var deck = require('./deck.js'),
   Evaluate = require('./evaluate'),
   $ = require('jquery');
 
-var dealHands = function dealHands () {
-  var toDom = '', hand, i = 0;
+var dealHands = function dealHands ($loading) {
+  var toDom = '',
+    hand, i = 0, j = 0;
 
-  while (i < 1000000) {
-    shuffle(deck);
-    hand = new Evaluate(deck.slice(0,5));
-    toDom += '<li>' + hand.handToString + ': ' + hand.result + '</li>';
-    i++;
+  for (; i < 100; i++) {
+    toDom = '';
+    j = 0;
+
+    while (j < 10000) {
+      shuffle(deck);
+      hand = new Evaluate(deck.slice(0,5));
+      toDom += '<li>' + hand.handToString + ': ' + hand.result + '</li>';
+      j++;
+    }
+
+    $loading.html((i + 1) + '%');
   }
-
-  return toDom;
 };
 
 $(function () {
-  var $list = $('ul.list'),
-    output = dealHands();
-  $list.append(output);
+  var $list = $('ol.list'),
+    $loading = $('div.loading'),
+    output = dealHands($loading);
+
+  $loading.hide();
+  $list.append(output).show();
 })
